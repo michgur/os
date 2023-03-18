12db696d60edd0c3108ffc0ebcc3eed68a8a47e0
michgur <michael@thegur.net>
2023-03-18 12:11:25 +0200

    added main

--- ex1/osm.cpp
+++ ex1/osm.cpp
@@ -8,61 +8,21 @@ double gettimeofdaynano() {
   return (tv.tv_sec * 1000000 + tv.tv_usec) * 1000.0;
 }
 
-void nop() {}
-
-// double osm_operation_time(unsigned int iterations) {
-//   if (iterations == 0) {
-//     return -1;
-//   }
-
-//   int a = 0;
-//   double start = gettimeofdaynano();
-//   for (unsigned int i = 0; i < iterations; i++) {
-//     a++;
-//   }
-//   return gettimeofdaynano() - start;
-// }
-
-// double osm_function_time(unsigned int iterations) {
-//   if (iterations == 0) {
-//     return -1;
-//   }
-
-//   double start = gettimeofdaynano();
-//   for (unsigned int i = 0; i < iterations; i++) {
-//     nop();
-//   }
-//   return gettimeofdaynano() - start;
-// }
-
-// double osm_syscall_time(unsigned int iterations) {
-//   if (iterations == 0) {
-//     return -1;
-//   }
-
-//   double start = gettimeofdaynano();
-//   for (unsigned int i = 0; i < iterations; i++) {
-//     OSM_NULLSYSCALL;
-//   }
-//   return gettimeofdaynano() - start;
-// }
-
-// use macro to avoid code duplication, and to avoid the overhead of function
-// calls in the loop.
 #define OSM_TIME(expr, iterations)                                             \
   ((iterations == 0) ? -1 : ({                                                 \
     double start = gettimeofdaynano();                                         \
-    for (unsigned int i = 0; i < iterations; i++) {                            \
+    for (; iterations > 0; iterations--) {                                     \
       expr;                                                                    \
     }                                                                          \
     gettimeofdaynano() - start;                                                \
   }))
 
-// rewritten to use OSM_TIME macro
 double osm_operation_time(unsigned int iterations) {
   int a = 0;
   return OSM_TIME(a++, iterations);
 }
+
+void nop() {}
 
 double osm_function_time(unsigned int iterations) {
   return OSM_TIME(nop(), iterations);
@@ -70,4 +30,11 @@ double osm_function_time(unsigned int iterations) {
 
 double osm_syscall_time(unsigned int iterations) {
   return OSM_TIME(OSM_NULLSYSCALL, iterations);
+}
+
+#include <iostream>
+void main() {
+  std::cout << "Op. time: " << osm_operation_time(1000000000) << std::endl;
+  std::cout << "Fn. time: " << osm_function_time(1000000000) << std::endl;
+  std::cout << "Syscall time: " << osm_syscall_time(1000000000) << std::endl;
 }
