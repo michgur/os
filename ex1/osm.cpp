ca21cbab65ccc358d2ac9432a911e207b4352143
michgur <michael@thegur.net>
2023-03-18 10:41:40 +0200

    expression

--- ex1/osm.cpp
+++ ex1/osm.cpp
@@ -10,39 +10,64 @@ double gettimeofdaynano() {
 
 void nop() {}
 
-double osm_operation_time(unsigned int iterations) {
-  if (iterations == 0) {
-    return -1;
-  }
+// double osm_operation_time(unsigned int iterations) {
+//   if (iterations == 0) {
+//     return -1;
+//   }
 
+//   int a = 0;
+//   double start = gettimeofdaynano();
+//   for (unsigned int i = 0; i < iterations; i++) {
+//     a++;
+//   }
+//   return gettimeofdaynano() - start;
+// }
+
+// double osm_function_time(unsigned int iterations) {
+//   if (iterations == 0) {
+//     return -1;
+//   }
+
+//   double start = gettimeofdaynano();
+//   for (unsigned int i = 0; i < iterations; i++) {
+//     nop();
+//   }
+//   return gettimeofdaynano() - start;
+// }
+
+// double osm_syscall_time(unsigned int iterations) {
+//   if (iterations == 0) {
+//     return -1;
+//   }
+
+//   double start = gettimeofdaynano();
+//   for (unsigned int i = 0; i < iterations; i++) {
+//     OSM_NULLSYSCALL;
+//   }
+//   return gettimeofdaynano() - start;
+// }
+
+// use macro to avoid code duplication, and to avoid the overhead of function
+// calls in the loop.
+#define OSM_TIME(expr, iterations)                                             \
+  ((iterations == 0) ? -1 : ({                                                 \
+    double start = gettimeofdaynano();                                         \
+    for (unsigned int i = 0; i < iterations; i++) {                            \
+      expr;                                                                    \
+    }                                                                          \
+    gettimeofdaynano() - start;                                                \
+  }))
+
+// rewritten to use OSM_TIME macro
+double osm_operation_time(unsigned int iterations) {
   int a = 0;
-  double start = gettimeofdaynano();
-  for (unsigned int i = 0; i < iterations; i++) {
-    a++;
-  }
-  return gettimeofdaynano() - start;
+  return OSM_TIME(a++, iterations);
 }
 
 double osm_function_time(unsigned int iterations) {
-  if (iterations == 0) {
-    return -1;
-  }
-
-  double start = gettimeofdaynano();
-  for (unsigned int i = 0; i < iterations; i++) {
-    nop();
-  }
-  return gettimeofdaynano() - start;
+  return OSM_TIME(nop(), iterations);
 }
 
 double osm_syscall_time(unsigned int iterations) {
-  if (iterations == 0) {
-    return -1;
-  }
-
-  double start = gettimeofdaynano();
-  for (unsigned int i = 0; i < iterations; i++) {
-    OSM_NULLSYSCALL;
-  }
-  return gettimeofdaynano() - start;
+  return OSM_TIME(OSM_NULLSYSCALL, iterations);
 }
