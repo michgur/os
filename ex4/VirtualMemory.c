f56327965c985c57daed8abb133399723d6d0f90
michgur <michael@thegur.net>
2023-06-14 20:11:10 +0300

    refactor

--- ex4/VirtualMemory.c
+++ ex4/VirtualMemory.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+/******************************* Constants ***********************************/
+
 #define FAIL_STATUS 0
 #define SUCCES_STATUS 1
 
@@ -11,41 +13,22 @@
 #define FRAME_INDEX(addr) (addr >> OFFSET_WIDTH)
 #define FRAME_ADDR(index) (index << OFFSET_WIDTH)
 
-int dec_to_bin(int n) {
-  int bin = 0;
-  int rem, i = 1;
-  while (n != 0) {
-    rem = n % 2;
-    n /= 2;
-    bin += rem * i;
-    i *= 10;
-  }
-  return bin;
-}
-
-// Stores a frame and the address of its pointer in physical memory
-typedef struct {
-  // frame index
-  uint64_t index;
-  // address in physical memory of pointer to frame
-  uint64_t addr;
-} frame_ptr_t;
-
-void clear_frame(uint64_t index) {
-  uint64_t addr = FRAME_ADDR(index);
-  for (int i = 0; i < PAGE_SIZE; i++) {
-    PMwrite(addr + i, 0);
-  }
-}
-
-void VMinitialize() { clear_frame(0); }
-
 /**
  * Translate virtual address to physical address
  * @param addr address to translate
  * @return 1 on success, 0 on failure
  */
 int translate(uint64_t *addr);
+
+/**
+ * Fills the given frame with zeros
+ * @param index index of frame to clear
+ */
+void clear_frame(uint64_t index);
+
+/******************************* API impl ***********************************/
+
+void VMinitialize() { clear_frame(0); }
 
 int VMread(uint64_t addr, word_t *value) {
   if (addr >= VIRTUAL_MEMORY_SIZE || translate(&addr) == FAIL_STATUS) {
@@ -65,6 +48,16 @@ int VMwrite(uint64_t addr, word_t value) {
   return SUCCES_STATUS;
 }
 
+/******************************* Frame methods *******************************/
+
+// Stores a frame and the address of its pointer in physical memory
+typedef struct {
+  // frame index
+  uint64_t index;
+  // address in physical memory of pointer to frame
+  uint64_t addr;
+} frame_ptr_t;
+
 uint64_t get_child(uint64_t node, int index) {
   word_t child;
   PMread(FRAME_ADDR(node) + index, &child);
@@ -81,6 +74,16 @@ void set_child(uint64_t node, int index, uint64_t child) {
   PMwrite(FRAME_ADDR(node) + index, child);
 }
 
+void clear_frame(uint64_t index) {
+  uint64_t addr = FRAME_ADDR(index);
+  for (int i = 0; i < PAGE_SIZE; i++) {
+    PMwrite(addr + i, 0);
+  }
+}
+
+/********************* Table tree traversal and translation *******************/
+
+// context for dfs execution
 typedef struct {
   // index of page to restore
   uint64_t page;
@@ -168,8 +171,16 @@ int dfs(dfs_context_t *context, frame_ptr_t node, int level,
   return update_context(context, node, level, is_empty, virtual_addr);
 }
 
+/**
+ * Finds a frame, evicts it if necessary and inserts the new page into it.
+ * @param parent_node the parent node of the page (used to avoid evicting it)
+ * @param index_in_parent the index of the page in the parent node
+ * @param page the page to insert
+ * @param is_leaf whether the page is a leaf
+ * @return the index of the frame containing the page (after insertion)
+ */
 uint64_t page_fault(uint64_t parent_node, int index_in_parent, uint64_t page,
-                    int level) {
+                    bool is_leaf) {
   dfs_context_t context = {page, parent_node, 0, 0, 0, 0};
   bool remove_from_parent = true;
 
@@ -192,12 +203,12 @@ uint64_t page_fault(uint64_t parent_node, int index_in_parent, uint64_t page,
   if (remove_from_parent) {
     PMwrite(frame.addr, 0);
   }
-  if (level + 1 < TABLES_DEPTH) {
-    // if next level is a table map, clear the frame
-    clear_frame(frame.index);
-  } else {
-    // otherwise, restore the page
+  if (is_leaf) {
+    // if next level is a page, restore it
     PMrestore(frame.index, page);
+  } else {
+    // otherwise, clear the frame
+    clear_frame(frame.index);
   }
   // write to new parent
   set_child(parent_node, index_in_parent, frame.index);
@@ -205,30 +216,35 @@ uint64_t page_fault(uint64_t parent_node, int index_in_parent, uint64_t page,
   return frame.index;
 }
 
-uint64_t find_frame(uint64_t page, uint64_t virtual_addr, uint64_t node,
-                    int level) {
+/**
+ * Finds the frame containing the page. If page is not in memory, evicts a frame
+ * and restores the page into it.
+ * @param page the page to insert
+ * @param node the current node
+ */
+uint64_t find_frame(uint64_t page, uint64_t node, int level) {
   // print node and level
   if (level == TABLES_DEPTH) {
     return node;
   }
 
   // bit index of where the current level offset starts
-  uint64_t bit_index = (level + 1) * OFFSET_WIDTH;
-  uint64_t offset =
-      (virtual_addr >> (VIRTUAL_ADDRESS_WIDTH - bit_index)) & OFFSET_MASK;
+  // we add 2 : once to skip the root and once to skip the current level
+  uint64_t bit_index = (level + 2) * OFFSET_WIDTH;
+  uint64_t offset = (page >> (VIRTUAL_ADDRESS_WIDTH - bit_index)) & OFFSET_MASK;
 
   uint64_t child = get_child(node, offset);
   if (child == 0) {
     // page fault
-    child = page_fault(node, offset, page, level);
+    child = page_fault(node, offset, page, level + 1 >= TABLES_DEPTH);
   }
 
-  return find_frame(page, virtual_addr, child, level + 1);
+  return find_frame(page, child, level + 1);
 }
 
 int translate(uint64_t *addr) {
   uint64_t offset = *addr & OFFSET_MASK;
   uint64_t page = *addr >> OFFSET_WIDTH;
-  *addr = FRAME_ADDR(find_frame(page, *addr, 0, 0)) + offset;
+  *addr = FRAME_ADDR(find_frame(page, 0, 0)) + offset;
   return (*addr == offset) ? FAIL_STATUS : SUCCES_STATUS;
 }
